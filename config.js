70ed6e98d5f0cd0298ca4a2bb9334e8fca27e051
DESKTOP-64PLAOK\maxime <bret.maxime@gmail.com>
2017-03-11 01:54:25 +0100

    no message

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    radio: {
+        nrj: "http://cdn.nrjaudio.fm/audio1/fr/40101/aac_576.mp3?origine=fluxradios",
+        franceInter: "http://direct.franceinter.fr/live/franceinter-midfi.mp3"
+    }
+};
